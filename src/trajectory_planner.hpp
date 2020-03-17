6b13523a4753a15a7c2c50d566014b13a2183a65
ValerioDiSano <valerio.disano@fcagroup.com>
2020-03-17 21:27:45 +0100

    feat: implemented ctor for TrajectoryPlanner class
    Class constructor implemented and added private data memebers

--- src/trajectory_planner.hpp
+++ src/trajectory_planner.hpp
@@ -1,12 +1,38 @@
 #ifndef TRAJECTORY_PLANNER_HPP_
 #define TRAJECTORY_PLANNER_HPP_
 
+#include <vector>
+
+using double_vec = std::vector<double>
+
 class TrajectoryPlanner
 {
-    public:
 
-    private:
+  public:
 
+    TrajectoryPlanner(
+            const double_vec& map_waypoints_x,
+            const double_vec& map_waypoints_y,
+            const double_vec& map_waypoints_s,
+            const size_t n_pts=50) :
+                map_waypoints_x_ {map_waypoints_x},
+                map_waypoints_y_ {map_waypoints_y},
+                map_waypoints_s_ {map_waypoints_s},
+                n_pts_ {n_pts}
+    {;}
+
+  private:
+    double_vec ref_pts_x;
+    double_vec ref_pts_y;
+
+    size_t n_pts_; 
+
+    double_vec next_x;
+    dobule_vec next_y;
+
+    double_vec& map_waypoints_x_;
+    double_vec& map_waypoints_y_;
+    double_vec& map_waypoints_s_;
 };
 
 #endif
