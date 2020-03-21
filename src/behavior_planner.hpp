4da7856fec4a8642b94535a0b24c6b170930a31e
ValerioDiSano <valerio.disano@fcagroup.com>
2020-03-21 14:45:26 +0100

    refactor: Move template function predictionStep from source to header file
    Since there was not possible to compile the template class member function
    if it was in the source file, the implementation has been moved to the header file.

--- src/behavior_planner.hpp
+++ src/behavior_planner.hpp
@@ -3,6 +3,7 @@
 
 #include "helpers.h"
 
+
 class BehaviorPlanner
 {
   public:
@@ -78,11 +79,46 @@ class BehaviorPlanner
     } Prediction;
     
     template <class SensorFusion>
-    Prediction predictionStep(const SensorFusion& sensor_fusion_data); 
+    Prediction predictionStep(const SensorFusion& sensor_fusion_data)
+    {
+    Prediction pred;
+
+        for(const auto& data : sensor_fusion_data)
+        {
+            double traffic_car_d = data[6];
+            double traffic_car_s = data[5];
+
+            int t_car_lane_id = Car::fromFrenet2LaneId(traffic_car_d);
+
+            if (t_car_lane_id == -1) continue; //no traffic car close to our vehicle
+            
+            auto other_car_speed = vectorMag2D(double(data[3]), double(data[4]));
+
+            // should estimate current s traffic car position
+            //
+            int relative_car_position = t_car_lane_id - conf().currentLane();
+            switch (relative_car_position)
+            {
+                case 0: pred.car_ahead = (traffic_car_s > conf().currentS());
+                        break;
+                case 1: pred.car_on_right = true;
+                        break;
+                case -1: pred.car_on_left = true;
+                        break;
+                default:
+                        break;
+            }
+        }
+    
+        this->last_prediction = pred;
+        return pred;
+    }
+    
     void computeBehavior() const;
 
   private:
     Prediction last_prediction;
+    static constexpr auto conf = &BehaviorPlanner::VehicleConfiguration::instance;
 };
 
 #endif
